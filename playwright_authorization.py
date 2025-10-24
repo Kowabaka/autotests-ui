db0c4b72159b16099732f5de3f73eb32be4afb2c
Kowabaka <102693359+Kowabaka@users.noreply.github.com>
2025-10-24 13:36:52 +0300

    Углубление в Playwright

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -1,17 +1,30 @@
-from pydoc import browse
-
 from playwright.sync_api import sync_playwright, expect
+
+# Запуск Playwright В синхронном режиме
 with sync_playwright() as playwright:
+    # Открываем браузер Chromium (не в headless режиме, чтобы видеть действия)
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
+
+    # Переходим на страницу авторизации
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-    email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
+
+    # Находим поле "Email" и заполняем его
+    email_input = page.get_by_test_id("login-form-email-input").locator("input")
     email_input.fill("user.name@gmail.com")
-    password_input = page.locator('//div[@data-testid="login-form-password-input"]//div//input')
+
+    # Находим поле "Password" и заполняем его
+    password_input = page.get_by_test_id('login-form-password-input').locator("input")
     password_input.fill("password")
-    login_button = page.locator('//button[@data-testid="login-page-login-button"]')
+
+    # Находим кнопку "Login" и кликаем на нее
+    login_button = page.get_by_test_id('login-page-login-button')
     login_button.click()
-    wrong_email_or_password_alert = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
+
+    # Проверяем, что появилось сообщение об ошибке
+    wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+
+    # Пауза на 5 секунд, чтобы увидеть результат
     page.wait_for_timeout(5000)
